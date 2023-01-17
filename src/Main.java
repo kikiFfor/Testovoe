a770d5b554ae9bf27c7b5f7749c60d7b3f2b744c
kikiFfor <konstanty.nikiforoff@yandex.ru>
2023-01-17 16:39:39 +0300

    Внес исправления по вводу данных

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,40 @@
 
+
 import java.util.Scanner;
+import java.lang.String;
+
 public class Main {
-
-    public static void main(String[] args) {
-
+        public static void main(String[] args) {
+        // Назначаем новый сканнер
         Scanner vvod = new Scanner(System.in);
 
-        System.out.println("Введите сумму взятых в кредит денег, ежемесячный платеж, процентную ставку, тип клиента ");
+        System.out.println("Введите сумму взятых в кредит денег, ежемесячный платеж, процентную ставку, тип клиента. Разделители - пробелы");
 
+        //Считываем введенные пользователям данные
         String parametrs = vvod.nextLine();
 
-        enum p  { String.split(parametrs) }
+
+        // Разбиваем строку на массив
+        String[] subparametrs = parametrs.split(" ");
 
 
-     /*   switch (type){
+        // Назначаем значения переменным-пар;
+
+        float Summa_kredita = Float.parseFloat(subparametrs[0]);
+        System.out.println("Сумма кредита, руб: " + Summa_kredita);
+
+        float Egemesyachniy_platej = Float.parseFloat(subparametrs[1]);
+        System.out.println("Ежемесячный платеж, руб: " + Egemesyachniy_platej);
+
+        float Procent = Float.parseFloat(subparametrs[2]);
+        System.out.println("Годовая процентная ставка, %: " + Procent);
+
+        String type_of_client = subparametrs[3];
+        System.out.println("Тип клиента: " + type_of_client);
+
+        //Выбор метода подсчета платы по кредиту
+
+        switch (type_of_client) {
             case ("human"):
                 Types_of_Credits.for_human();
                 break;
@@ -22,7 +43,10 @@ public class Main {
                 Types_of_Credits.for_Business();
                 break;
             default:
-                throw new IllegalStateException("Unexpected value: " + type);*/
+                System.out.println("Неверно указан тип клиента: " + type_of_client);
+                break;
         }
 
+
     }
+}
